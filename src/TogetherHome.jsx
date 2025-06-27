786f5dc6a7f6626d5be9cda8fedd3384721a829c
Together Founders <anisha@shecapital.vc>
2025-06-27 16:46:30 +0530

    Add files via upload

---
+++ src/TogetherHome.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function TogetherHome() {
+  return (
+    <div>
+      <h1>Together: India’s Most Powerful Female Founder Network</h1>
+      <p>This is your startup's front door. Welcome!</p>
+    </div>
+  );
+}
