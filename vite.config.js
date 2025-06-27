27a6c820e18a1cc5ec5c39e503f0ff1e4852c20b
Together Founders <anisha@shecapital.vc>
2025-06-27 16:53:48 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  root: '.',
+  root: '.', // 🔥 ensures it's building from the root folder
   build: {
     outDir: 'dist',
   },
-})
+});
